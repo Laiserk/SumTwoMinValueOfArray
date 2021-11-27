a639f8b01065e1ecba1402080977ac7a7142ce00
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2021-11-27 16:05:18 +0500

    Creat metod SumTwoMinValue

--- SumTwoMinValueOfArrayLib/ArrayСalculations.cs
+++ SumTwoMinValueOfArrayLib/ArrayСalculations.cs
@@ -6,7 +6,34 @@ namespace ArrayProcessingLib
     {
         public double SumTwoMinValues(double[] array) 
         {
-            return 0;
+            int min1, min2, tmp;
+            if (array[0] < array[1])
+            {
+                min1 = 0;
+                min2 = 1;
+            }
+            else
+            {
+                min1 = 1;
+                min2 = 0;
+            }
+            for(int i = 2; i<array.Length; i++)
+            {
+                if(array[i] < array[min1])
+                {
+                    tmp = min1;
+                    min1 = i;
+                    if (array[tmp] < array[min2])
+                        min2 = tmp;
+                }
+                else
+                {
+                    if (array[i] < array[min2])
+                        min2 = i;
+                }
+            }
+            
+            return array[min1] + array[min2];
         }
     }
 }
