cf90784171390c52c29f7fd254d599896cdb0acc
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2021-11-29 03:18:23 +0500

    Add new tests for array

---
+++ SumTwoMinValueOfArrayTestsLib/CheckArrayTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ArrayProcessingLib;
+
+namespace SumTwoMinValueTestsLib
+{
+    [TestClass]
+    public class CheckArrayTests
+    {
+        [TestMethod]
+        public void TestArrayEmpty()
+        {
+            //arrange
+            string[] array = { };
+            string expected = "Array is empty";
+            ;
+            //act
+            var calc = new ArrayСalculations();
+            string actual = calc.SumTwoMinValues(array);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestArrayWithNonNumericValues()
+        {
+            //arrange
+            string[] array = { "1,1", "2,2", "!", "4", "5,5" };
+            string expected = "Some values cannot be processed";
+            //act
+            var calc = new ArrayСalculations();
+            string actual = calc.SumTwoMinValues(array);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestArrayEmptyValues()
+        {
+            //arrange
+            string[] array = { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+            string expected = "Some values cannot be processed";
+            //act
+            var calc = new ArrayСalculations();
+            string actual = calc.SumTwoMinValues(array);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
