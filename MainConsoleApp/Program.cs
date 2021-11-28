3d77732c0cf9cfe47bf96cc7907f8da496db3d31
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2021-11-29 03:17:11 +0500

    add negative values

--- MainConsoleApp/Program.cs
+++ MainConsoleApp/Program.cs
@@ -4,29 +4,29 @@ namespace MainConsoleApp
 {
     class Program
     {
-        public static double[] BuildArray()
+        public static string[] BuildArray()
         {
             var rand = new Random();
             int len = rand.Next(5, 20);
-            double[] array = new double[len];
+            string[] array = new string[len];
             
             for (int i = 0; i < array.Length; i++)
-            {     
-                array[i] = Math.Round(rand.NextDouble(), 2) + rand.Next(1, 10);
+            {
+                int sign = rand.Next(-1, 1);
+                array[i] = (sign != 0 ? sign * Math.Round(rand.NextDouble() + rand.Next(0, 10), 3) : Math.Round(rand.NextDouble() + rand.Next(0, 10), 3)).ToString();
             }
 
             return array;
         }
         static void Main(string[] args)
         {
-            double[] arr = BuildArray();
+            string[] arr = BuildArray();
 
-            foreach(double item in arr)
+            foreach(var item in arr)
             {
                 Console.Write($"{item} ");
             }
             Console.WriteLine();
-
             var ac = new ArrayСalculations();
             Console.WriteLine(ac.SumTwoMinValues(arr));
 
