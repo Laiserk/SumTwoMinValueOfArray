f2cd8a83e624cab3ef2b7fabe07b4e4e21fa0f4a
Oleg Sevostyanov <savosarenskyrim@gmail.com>
2021-11-29 03:17:39 +0500

    add metod CheckArray

--- SumTwoMinValueOfArrayLib/ArrayСalculations.cs
+++ SumTwoMinValueOfArrayLib/ArrayСalculations.cs
@@ -4,10 +4,33 @@ namespace ArrayProcessingLib
 {
     public class ArrayСalculations
     {
-        public double SumTwoMinValues(double[] array) 
+        public string CheckArray(string[] array)
         {
+            if (array.Length == 0)
+                return "Array is empty";
+
+            double digit;
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (!double.TryParse(array[i], out digit))
+                    return "Some values cannot be processed";
+            }
+            return "correctly";
+        }
+
+        public string SumTwoMinValues(string[] array)
+        {
+            if (CheckArray(array) != "correctly")
+                return CheckArray(array);
+
             int min1, min2, tmp;
-            if (array[0] < array[1])
+            double[] darray = new double[array.Length];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                darray[i] = double.Parse(array[i]);
+            }
+            if (darray[0] < darray[1])
             {
                 min1 = 0;
                 min2 = 1;
@@ -17,23 +40,24 @@ namespace ArrayProcessingLib
                 min1 = 1;
                 min2 = 0;
             }
-            for(int i = 2; i<array.Length; i++)
+
+            for (int i = 2; i < array.Length; i++)
             {
-                if(array[i] < array[min1])
+                if (darray[i] < darray[min1])
                 {
                     tmp = min1;
                     min1 = i;
-                    if (array[tmp] < array[min2])
+                    if (darray[tmp] < darray[min2])
                         min2 = tmp;
                 }
                 else
                 {
-                    if (array[i] < array[min2])
+                    if (darray[i] < darray[min2])
                         min2 = i;
                 }
             }
-            
-            return array[min1] + array[min2];
+
+            return Math.Round(darray[min1] + darray[min2], 3).ToString();
         }
     }
 }
